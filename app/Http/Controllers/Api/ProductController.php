cd374fe30a866b61e31cd9105ad854763bf2fedd
cristaldevil <cristaldevil@mail.ru>
2025-02-04 19:20:17 +0700

    Начат модуль Product

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -3,9 +3,38 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\ProductRequest;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    // Получение данных о всех товарах
+    public function index() {
+        $products = Product::all();
+        return response()->json($products)->setStatusCode(200);
+    }
+
+    // Получение данных о конкретном товаре (для ресурсного маршрута)
+    public function show(Product $product) {
+        return response()->json($product)->setStatusCode(200);
+    }
+
+    // Создание нового товара
+    public function store(ProductRequest $request) {
+        $product = Product::create($request->validated());
+        return response()->json($product)->setStatusCode(201);
+    }
+
+    // Обновление товара (для ресурсного маршрута)
+    public function update(ProductRequest $request, Product $product) {
+        $product->update($request->validated());
+        return response()->json($product)->setStatusCode(200);
+    }
+
+    // Удаление товара (для ресурсного маршрута)
+    public function destroy(Product $product) {
+        $product->delete();
+        return response()->json()->setStatusCode(204);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use \App\Http\Controllers\Api\AuthController;
@@ -18,4 +19,5 @@ Route::delete('/category/{id}', [CategoryController::class, 'destroy']);
 */
 
 Route::apiResource('/category', CategoryController::class);
+Route::apiResource('/product', ProductController::class);
 
